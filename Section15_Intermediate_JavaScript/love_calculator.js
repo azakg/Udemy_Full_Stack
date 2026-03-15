dac59031a55645570cdf047cd3767575c9596a1d
azakg <azakgzuu@gmail.com>
2026-03-15 11:40:57 -0400

    Love calculator

---
+++ Section15_Intermediate_JavaScript/love_calculator.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Type first name: ");
+var secondName = prompt("Type secong name: ");
+var rNumber = Math.random();
+var n = rNumber*100+1;
+
+alert(firstName + " matches to " + secondName + " for " + Math.floor(n)+"%.");
---
+++ Section15_Intermediate_JavaScript/random.js
@@ -0,0 +1,3 @@
+var rNumber = Math.random();
+var n = rNumber*10+1;
+console.log(Math.floor(n));
