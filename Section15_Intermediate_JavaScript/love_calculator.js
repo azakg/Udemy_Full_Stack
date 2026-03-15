bfb12b9291cdbc3a32cdd22b84c8cfdc6f2e0a34
azakg <azakgzuu@gmail.com>
2026-03-15 11:49:39 -0400

    Second version Love Calculator

--- Section15_Intermediate_JavaScript/love_calculator.js
+++ Section15_Intermediate_JavaScript/love_calculator.js
@@ -3,4 +3,8 @@ var secondName = prompt("Type secong name: ");
 var rNumber = Math.random();
 var n = rNumber*100+1;
 
-alert(firstName + " matches to " + secondName + " for " + Math.floor(n)+"%.");
+if (n > 70){
+    alert(firstName + " matches to " + secondName + " for " + Math.floor(n)+"%. this is truly love");
+} else {
+    alert(firstName + " matches to " + secondName + " for " + Math.floor(n)+"%.");
+}
