a2fa2269933d1cf7c66ec986592c65d70be32088
azakg <azakgzuu@gmail.com>
2026-03-15 12:01:27 -0400

    v3 Love Calculator

--- Section15_Intermediate_JavaScript/love_calculator.js
+++ Section15_Intermediate_JavaScript/love_calculator.js
@@ -5,6 +5,10 @@ var n = rNumber*100+1;
 
 if (n > 70){
     alert(firstName + " matches to " + secondName + " for " + Math.floor(n)+"%. this is truly love");
-} else {
+} 
+if (n > 30 && n < 70){
     alert(firstName + " matches to " + secondName + " for " + Math.floor(n)+"%.");
+}
+if (n <=30){
+    alert(firstName + " and " + secondName + " is like oil and water. Your love score is: " + Math.floor(n)+"%.");
 }
