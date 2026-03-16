0ba85e421e8becb55b283ed2fb6998aa4ed73637
azakg <azakgzuu@gmail.com>
2026-03-16 10:45:58 -0400

    Random Name

---
+++ Section15_Intermediate_JavaScript/randomChoose.js
@@ -0,0 +1,10 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+var nameList = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+var randNum = Math.floor((Math.random())*(nameList.length));
+console.log(nameList[randNum]+" is going to buy lunch today!");
+/******Don't change the code below*******/    
+}
