367fa89e2fe5a088de6e06f7c4c4765027bd2fc3
azakg <azakgzuu@gmail.com>
2026-03-17 15:49:17 -0400

    Drum Udemy Version

--- Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
+++ Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
@@ -1,11 +1,19 @@
-document.querySelectorAll("button")[0].addEventListener("click", handleClick);
-document.querySelectorAll("button")[1].addEventListener("click", handleClick);
-document.querySelectorAll("button")[2].addEventListener("click", handleClick);
-document.querySelectorAll("button")[3].addEventListener("click", handleClick);
-document.querySelectorAll("button")[4].addEventListener("click", handleClick);
-document.querySelectorAll("button")[5].addEventListener("click", handleClick);
-document.querySelectorAll("button")[6].addEventListener("click", handleClick);
+// document.querySelectorAll("button")[0].addEventListener("click", handleClick);
+// document.querySelectorAll("button")[1].addEventListener("click", handleClick);
+// document.querySelectorAll("button")[2].addEventListener("click", handleClick);
+// document.querySelectorAll("button")[3].addEventListener("click", handleClick);
+// document.querySelectorAll("button")[4].addEventListener("click", handleClick);
+// document.querySelectorAll("button")[5].addEventListener("click", handleClick);
+// document.querySelectorAll("button")[6].addEventListener("click", handleClick);
 
-function handleClick(){
-    alert("Test");
+var numberOfDromBottons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i<numberOfDromBottons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert("I got clicked!");
+    });
 }
+
+// function handleClick(){
+//     alert("Test");
+// }
