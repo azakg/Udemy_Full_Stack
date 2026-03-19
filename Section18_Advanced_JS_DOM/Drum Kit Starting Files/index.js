1b73eb44c2f0ef5bddf7c754e9c7b1aa2e3ce635
azakg <azakgzuu@gmail.com>
2026-03-19 12:51:55 -0400

    My version of the Drum Game

--- Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
+++ Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
@@ -1,8 +1,36 @@
 
 var numberOfDromBottons = document.querySelectorAll(".drum").length;
 
-for(var i = 0; i<numberOfDromBottons; i++){
+
+for(let i = 0; i<numberOfDromBottons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        alert("I got clicked!");
+        if(i === 0){
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+        }else if(i === 1){
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+        }
+        else if(i === 2){
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+        }
+        else if(i === 3){
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+        }
+        else if(i === 4){
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+        }
+        else if(i === 5){
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+        }
+        else if(i === 6){
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+        }
+        
     });
 }
