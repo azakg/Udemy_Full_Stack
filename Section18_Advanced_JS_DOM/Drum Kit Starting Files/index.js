7eae8b1735f6cf6c50d6bce9a7127651802c768d
azakg <azakgzuu@gmail.com>
2026-03-19 12:05:00 -0400

    March 19 Morning

--- Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
+++ Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
@@ -1,10 +1,3 @@
-// document.querySelectorAll("button")[0].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[1].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[2].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[3].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[4].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[5].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[6].addEventListener("click", handleClick);
 
 var numberOfDromBottons = document.querySelectorAll(".drum").length;
 
@@ -13,7 +6,3 @@ for(var i = 0; i<numberOfDromBottons; i++){
         alert("I got clicked!");
     });
 }
-
-// function handleClick(){
-//     alert("Test");
-// }
