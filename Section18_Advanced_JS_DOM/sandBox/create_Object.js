b52bed376b49b125b7e1ee8212c2313e74c9ef26
azakg <azakgzuu@gmail.com>
2026-03-23 12:25:29 -0400

    Object creation

--- Section18_Advanced_JS_DOM/sandBox/create_Object.js
+++ Section18_Advanced_JS_DOM/sandBox/create_Object.js
@@ -2,5 +2,10 @@ function houseKeeper(name, age, workPermission, languages){
 	this.name = name;
 	this.age = age;
 	this.workPermission = workPermission;
-	this.languages = languages;	
+	this.languages = languages;
+	this.clearningFunction = function(){
+		alert("Clearning in progress...");
+	}
 }
+
+var hk1 = new houseKeeper("Anna", 25, true, ["English", "Spane"]);
