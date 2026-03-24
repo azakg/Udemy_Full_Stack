7b4e90745285e35d8dc20a73ffbcdcc8c93dd455
azakg <azakgzuu@gmail.com>
2026-03-24 19:57:03 -0400

    Animation with Jquery, add/remove class/attributes

--- Section19_JQuery/index.js
+++ Section19_JQuery/index.js
@@ -1,1 +1,15 @@
 $("h1").addClass("big-title margin-50");
+$("button").eq(0).text("Hi");
+
+$("button").eq(0).click(function () {
+    if ($("img").attr("src") === "./images/tom2.png") {
+        $("img").attr("src", "./images/tom1.png");
+    }
+    else {
+        $("img").attr("src", "./images/tom2.png");
+    }
+});
+
+$("h1").click(function (){
+    $("h1").removeClass("big-title");
+})
