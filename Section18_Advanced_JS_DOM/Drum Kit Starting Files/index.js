7f5bd8ad744fb3f689eb21cdaae23c2808a5a939
azakg <azakgzuu@gmail.com>
2026-03-24 15:21:15 -0400

    My Version of button animation

--- Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
+++ Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
@@ -7,6 +7,7 @@ for(let i = 0; i<numberOfDromBottons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
@@ -14,6 +15,8 @@ for(let i = 0; i<numberOfDromBottons; i++){
 
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
+    
 });
 
 function makeSound(key){
@@ -21,6 +24,48 @@ function makeSound(key){
                 case "w":
                     var audio = new Audio("./sounds/tom-1.mp3");
                     audio.play();
+                    break;
+                case "a":
+                    var audio = new Audio("./sounds/tom-2.mp3");
+                    audio.play();
+                    break;
+                case "s":
+                    var audio = new Audio("./sounds/tom-3.mp3");
+                    audio.play();
+                    break;
+                case "d":
+                    var audio = new Audio("./sounds/tom-4.mp3");
+                    audio.play();
+                    break;
+                case "j":
+                    var audio = new Audio("./sounds/snare.mp3");
+                    audio.play();
+                    break;
+                case "k":
+                    var audio = new Audio("./sounds/kick-bass.mp3");
+                    audio.play();
+                    break;
+                case "l":
+                    var audio = new Audio("./sounds/crash.mp3");
+                    audio.play();
+                    break;
+
+                default:
+                    break;
+            }
+}
+
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+        switch (currentKey) {
+                case "w":
+                    var element = document.querySelector(".w");
+                    element.classList.add("pressed"); 
+                    setTimeout(() => {
+                    element.classList.remove("pressed");
+                    }, 500);
+                    
                     break;
                 case "a":
                     var audio = new Audio("./sounds/tom-2.mp3");
