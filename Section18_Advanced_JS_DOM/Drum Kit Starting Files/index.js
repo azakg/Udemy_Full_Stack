78f3cd1001c205f0c34c21e63bc7d058580d759e
azakg <azakgzuu@gmail.com>
2026-03-24 15:33:25 -0400

    Udemy version of Button animation

--- Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
+++ Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
@@ -58,43 +58,10 @@ function makeSound(key){
 
 function buttonAnimation(currentKey){
     var activeButton = document.querySelector("."+currentKey);
-        switch (currentKey) {
-                case "w":
-                    var element = document.querySelector(".w");
-                    element.classList.add("pressed"); 
-                    setTimeout(() => {
-                    element.classList.remove("pressed");
-                    }, 500);
-                    
-                    break;
-                case "a":
-                    var audio = new Audio("./sounds/tom-2.mp3");
-                    audio.play();
-                    break;
-                case "s":
-                    var audio = new Audio("./sounds/tom-3.mp3");
-                    audio.play();
-                    break;
-                case "d":
-                    var audio = new Audio("./sounds/tom-4.mp3");
-                    audio.play();
-                    break;
-                case "j":
-                    var audio = new Audio("./sounds/snare.mp3");
-                    audio.play();
-                    break;
-                case "k":
-                    var audio = new Audio("./sounds/kick-bass.mp3");
-                    audio.play();
-                    break;
-                case "l":
-                    var audio = new Audio("./sounds/crash.mp3");
-                    audio.play();
-                    break;
-
-                default:
-                    break;
-            }
+      activeButton.classList.add("pressed");
+      setTimeout(() => {
+        activeButton.classList.remove("pressed");
+      },100);
 }
 // document.addEventListener("keypress", function(event){
   
