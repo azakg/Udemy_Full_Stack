9c78224fca4872c18dbf9cec07d6aa787cb42e15
azakg <azakgzuu@gmail.com>
2026-03-24 12:54:38 -0400

    Udemy versionof Drum Keypress

--- Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
+++ Section18_Advanced_JS_DOM/Drum Kit Starting Files/index.js
@@ -1,77 +1,86 @@
 
 var numberOfDromBottons = document.querySelectorAll(".drum").length;
+//Detecting Button press
 
 
 for(let i = 0; i<numberOfDromBottons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        
-
         var buttonInnerHTML = this.innerHTML;
-
-        switch (buttonInnerHTML) {
-            case "w":
-                var audio = new Audio("./sounds/tom-1.mp3");
-                audio.play();
-                break;
-            case "a":
-                var audio = new Audio("./sounds/tom-2.mp3");
-                audio.play();
-                break;
-            case "s":
-                var audio = new Audio("./sounds/tom-3.mp3");
-                audio.play();
-                break;
-            case "d":
-                var audio = new Audio("./sounds/tom-4.mp3");
-                audio.play();
-                break;
-            case "j":
-                var audio = new Audio("./sounds/snare.mp3");
-                audio.play();
-                break;
-            case "k":
-                var audio = new Audio("./sounds/kick-bass.mp3");
-                audio.play();
-                break;
-            case "l":
-                var audio = new Audio("./sounds/crash.mp3");
-                audio.play();
-                break;
-
-            default:
-                break;
-        }
+        makeSound(buttonInnerHTML);
     });
 }
 
+//Detecting Keyboard Press
+
 document.addEventListener("keypress", function(event){
-  
-   if (event["key"] === "w"){
-            var audio = new Audio("./sounds/tom-1.mp3");
-            audio.play();
-   }
-   else if (event["key"] === "a"){
-            var audio = new Audio("./sounds/tom-2.mp3");
-            audio.play();
-   }
-   else if (event["key"] === "s"){
-                var audio = new Audio("./sounds/tom-3.mp3");
-                audio.play();
-   }
-   else if (event["key"] === "d"){
-                var audio = new Audio("./sounds/tom-4.mp3");
-                audio.play();
-   }
-   else if (event["key"] === "j"){
-                var audio = new Audio("./sounds/snare.mp3");
-                audio.play();
-   }
-   else if (event["key"] === "k"){
-                var audio = new Audio("./sounds/kick-bass.mp3");
-                audio.play();
-   }
-   else if (event["key"] === "l"){
-                var audio = new Audio("./sounds/crash.mp3");
-                audio.play();
-   }
+    makeSound(event.key);
 });
+
+function makeSound(key){
+    switch (key) {
+                case "w":
+                    var audio = new Audio("./sounds/tom-1.mp3");
+                    audio.play();
+                    break;
+                case "a":
+                    var audio = new Audio("./sounds/tom-2.mp3");
+                    audio.play();
+                    break;
+                case "s":
+                    var audio = new Audio("./sounds/tom-3.mp3");
+                    audio.play();
+                    break;
+                case "d":
+                    var audio = new Audio("./sounds/tom-4.mp3");
+                    audio.play();
+                    break;
+                case "j":
+                    var audio = new Audio("./sounds/snare.mp3");
+                    audio.play();
+                    break;
+                case "k":
+                    var audio = new Audio("./sounds/kick-bass.mp3");
+                    audio.play();
+                    break;
+                case "l":
+                    var audio = new Audio("./sounds/crash.mp3");
+                    audio.play();
+                    break;
+
+                default:
+                    break;
+            }
+}
+// document.addEventListener("keypress", function(event){
+  
+//    if (event["key"] === "w"){
+//             var audio = new Audio("./sounds/tom-1.mp3");
+//             audio.play();
+//    }
+//    else if (event["key"] === "a"){
+//             var audio = new Audio("./sounds/tom-2.mp3");
+//             audio.play();
+//    }
+//    else if (event["key"] === "s"){
+//                 var audio = new Audio("./sounds/tom-3.mp3");
+//                 audio.play();
+//    }
+//    else if (event["key"] === "d"){
+//                 var audio = new Audio("./sounds/tom-4.mp3");
+//                 audio.play();
+//    }
+//    else if (event["key"] === "j"){
+//                 var audio = new Audio("./sounds/snare.mp3");
+//                 audio.play();
+//    }
+//    else if (event["key"] === "k"){
+//                 var audio = new Audio("./sounds/kick-bass.mp3");
+//                 audio.play();
+//    }
+//    else if (event["key"] === "l"){
+//                 var audio = new Audio("./sounds/crash.mp3");
+//                 audio.play();
+//    }
+// });
+
+
