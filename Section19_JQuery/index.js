8eb156b60a1d7ee097df257be97e9537dbddd12c
azakg <azakgzuu@gmail.com>
2026-03-24 21:56:12 -0400

    before/after/prepend/append

--- Section19_JQuery/index.js
+++ Section19_JQuery/index.js
@@ -25,3 +25,8 @@ $("h1").on("mouseover", function(){
 $("h1").on("mouseout", function(){
     $("h1").css("color", "green");
 });
+
+$("h1").before("<button>before</button>");
+$("h1").after("<button>after</button>");
+$("h1").prepend("<button>prepend</button>");
+$("h1").append("<button>append</button>");
