d68f6182d6462fad29986563a6a41f71d63c246e
azakg <azakgzuu@gmail.com>
2026-03-25 15:17:05 -0400

    PreGame Logic Commit

--- Section20_The_Simon_Game/Simon Game Challenge Starting Files/game.js
+++ Section20_The_Simon_Game/Simon Game Challenge Starting Files/game.js
@@ -1,30 +1,45 @@
+//##############################################################################
+//Initial 
 var gamePattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var nu1 = nextSequence();
 
+var randomChosenColour = buttonColours[nu1];
+gamePattern.push(buttonColours[nextSequence()]);
 
+var userChosenColour = [];
 
+//##############################################################################
+// Functions
+// Random number
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
     return randomNumber;
 }
+// Play audio
+function audioPlay(path){
+    var audio = new Audio(path);
+    audio.play();
+}
 
 
-var randomChosenColour = buttonColours[nu1];
-gamePattern.push(buttonColours[nextSequence()]);
+//#######################################################################################################################
+// Fill var userChosenColour = []
+$(".btn").click(function(){
+userChosenColour.push($(this).attr("id"));
+console.log(userChosenColour);
+});
 
 
-// console.log(randomChosenColour);
-// $("btn "+randomChosenColour).on("click", function(){
-//     $("h1").animate({opacity: 0.5});
-// })
-console.log(randomChosenColour);
-console.log(nu1);
+
+//#######################################################################################################################
+// Button click animation
 $("."+randomChosenColour).on("click", function(){
     $("."+randomChosenColour).animate({opacity: 0.5});
     setTimeout($("."+randomChosenColour).animate({opacity: 1}), 50);
-    var audio = new Audio("./sounds/"+randomChosenColour+'.mp3');
-    audio.play();
+
+    audioPlay("./sounds/"+randomChosenColour+'.mp3');// play sound
+
 })
