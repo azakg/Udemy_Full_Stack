e7524723e92874b3650ccaf72a221feefe5dc32a
azakg <azakgzuu@gmail.com>
2026-03-26 10:54:38 -0400

    QR Micro Project from GPT

---
+++ Section23_NodeJS/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+//var generatedName = require("sillyname");
+
+// import generatedName from "sillyname";
+
+// var sillyName = generatedName();
+// console.log(sillyName);
+
+import {randomSuperhero} from 'superheroes';
+
+const name = randomSuperhero();
+console.log("My name is "+name);
