9f08d19bf742eb79dc5a91ff8c93015319e2be27
azakg <azakgzuu@gmail.com>
2026-04-12 15:55:05 -0400

    create Section30_Build_your_own_API

--- SandBox/Section28_API/index.js
+++ SandBox/Section28_API/index.js
@@ -1,3 +1,11 @@
 import express from "express";
 import axios from "axios";
 
+const app = express();
+const port = 3001;
+
+app.use(express.static("public"));
+
+app.get("/", async(req, res)=>{
+    
+})
